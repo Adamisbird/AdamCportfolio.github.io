448fba43254059a2038bdd0504691d870f3df3a3
Adamisbird <156868567+Adamisbird@users.noreply.github.com>
2026-02-22 23:53:40 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Sets the footer year automatically
+// Sets the footer year automatically (only works if you have an element with id="year")
 document.addEventListener("DOMContentLoaded", () => {
   const yearEl = document.getElementById("year");
   if (yearEl) yearEl.textContent = String(new Date().getFullYear());
